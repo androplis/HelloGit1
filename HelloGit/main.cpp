6d3a0ca6adfdf48fc3b8aa5d4973982be1c4c052
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-10-21 09:35:04 -0400

    Initial Commit

---
+++ HelloGit/main.cpp
@@ -0,0 +1,13 @@
+// Andrew Biddle
+// CIS 1202.501
+// 10/21/2020
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World!";
+    cout << endl << endl;
+    return 0;
+}
